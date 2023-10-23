f552dd167e28181bac0f03c44ca2dbdfd80adf75
Yury1981 <109158155+Yury1981@users.noreply.github.com>
2023-10-24 00:11:43 +0300

    Delete Lesson 7.3.py

--- Lesson 7.3.py
+++
@@ -1,5 +0,0 @@
-print((1024).to_bytes(2, byteorder='big'))
-print((1024).to_bytes(10, byteorder='big'))
-print((-1024).to_bytes(10, byteorder='big', signed=True))
-x = 1000
-print(x.to_bytes((x.bit_length() // 8) + 1, byteorder='little'))
